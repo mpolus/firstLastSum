1b5a18c817b7704217f44ac7aca2e90d22a8812d
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-06 12:53:48 -0400

    sum

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumFirstAndLastDigit(252));
+        System.out.println(sumFirstAndLastDigit(257));
+        System.out.println(sumFirstAndLastDigit(0));
+        System.out.println(sumFirstAndLastDigit(5));
+        System.out.println(sumFirstAndLastDigit(-10));
+
+    }
+
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0){
+            return -1;
+        }
+        int lastDigit = number % 10;
+
+        int firstDigit = 0;
+        while (number > 0){
+            firstDigit = number;
+            number /= 10;
+        }
+        return firstDigit + lastDigit;
+
+    }
 }
